567b5a8b7206fd107e7414846a52d478c68bddf5
root <root@vps28104.public.cloudvps.com>
2014-05-25 14:22:45 +0200

    Introduced error handling of Xively API

--- xivelyDownload.py
+++ xivelyDownload.py
@@ -8,16 +8,20 @@ out = open('./outPut.csv', 'a')
 
 cache = ""
 while (time.mktime(datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()) < time.mktime(datetime.datetime.strptime(stop, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple())):
-	url = 'https://api.xively.com/v2/feeds/' + feedID + '.csv?start=' + start + '&end=' + str(datetime.datetime.fromtimestamp(time.mktime(datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()) + 6*3600).strftime('%Y-%m-%dT%H:%M:%S.%fZ')) + '&limit=1000&interval=0'
+	url = 'https://api.xively.com/v2/feeds/' + feedID + '.csv?start=' + start + '&end=' + str(datetime.datetime.fromtimestamp(time.mktime(datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()) + 5*3600).strftime('%Y-%m-%dT%H:%M:%S.%fZ')) + '&limit=1000&interval=0'
 	page = requests.get(url, auth=(login, pwd))
 # xively returns the boundary values twice
 	lines = page.content.split('\n')
 	for line in lines:
-#		out.write(line.split(',')[1] + ',' + line.split(',')[2] + '\n')
-		out.write(line + '\n')
+		splitline = line.split(',')
+		if len(splitline)!= 3 :
+			print  len(splitline) , line
+		out.write(splitline[0]+ ',' + 
+			datetime.datetime.strptime(splitline[1], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S") + 
+			',' + splitline[2] + '\n')
+#		out.write(line + '\n')
 		cache = line.split(',')[1]
 		
 #	start = cache.split('.')[0] + 'Z'
 	start = cache
-
 out.close()
