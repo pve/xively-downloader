672b35bce46430626789f3dc52376da287c9b117
Peter van Eijk <pveijk@gmail.com>
2014-08-19 17:07:13 +0200

    fixed bug when an interval results in empty data.

--- xivelyDownload.py
+++ xivelyDownload.py
@@ -7,9 +7,16 @@ import time
 out = open('./outPut.csv', 'a')
 
 cache = ""
+endstring = "" 
 while (time.mktime(datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()) < time.mktime(datetime.datetime.strptime(stop, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple())):
-	url = 'https://api.xively.com/v2/feeds/' + feedID + '.csv?start=' + start + '&end=' + str(datetime.datetime.fromtimestamp(time.mktime(datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()) + 5*3600).strftime('%Y-%m-%dT%H:%M:%S.%fZ')) + '&limit=1000&interval=0'
+	endstring = str(datetime.datetime.fromtimestamp(time.mktime(datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()) + 5*3600).strftime('%Y-%m-%dT%H:%M:%S.%fZ'))
+	url = 'https://api.xively.com/v2/feeds/' + feedID + '.csv?start=' + start + '&end=' + endstring + '&limit=1000&interval=0'
+	start = endstring # if we get zero results, we want to move to the next window.
+# the remaining assumption is that the timestamps of our answers are strictly greater than 'start'
 	page = requests.get(url, auth=(login, pwd))
+	if page.status_code != 200 :
+		print page.status_code 
+	page.raise_for_status()
 # xively returns the boundary values twice
 	lines = page.content.split('\n')
 	for line in lines:
